58a6c68793ad98dd17e73baef0ac697bf49d6e65
myselfregmi <pushparegmi0@gmail.com>
2023-07-04 17:32:06 +0545

    Create the todo-list using typescripts

---
+++ day20/ts/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Todo from './assets/components/Todo'
+
+
+const App = () => {
+ 
+  
+  return (
+    <div className='bg-blue-200 h-screen'>
+      <Todo/>
+    </div>
+  )
+}
+
+export default App
